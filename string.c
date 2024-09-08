3be81c9dc14f728af02fbe6692fd7d3b17ce8c07
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-08 21:58:59 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdio.h>
+void main()
+{
+int n,i;
+char str[50];
+printf("enter n");
+scanf("%d",&n);
+printf("enter string");
+scanf("%s",&str);
+printf("%s",str);
+}
